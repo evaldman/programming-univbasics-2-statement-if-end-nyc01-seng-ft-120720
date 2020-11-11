0e9f07e2e40c4a441f546b6cece153902461c919
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-11 19:20:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,10 +5,10 @@
 # end
 # puts "Code after if...end"
 
-chance_of_rain = 0.3
+chance_of_rain = 0.2
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter!"
-  elsif (chance_of_rain 0.25 && chance_of_rain < 0.75)
+  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "Pack an umbrella!"
 else
   puts "Stay at home and read Hegel."
