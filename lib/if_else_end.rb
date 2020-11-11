3da4ed8f16485ba367583a9615fbc036cdf75d32
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-11 19:49:06 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,29 @@
-# Write your solution here
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+
+# chance_of_rain = 0.3
+# if chance_of_rain <= 0.25
+#  puts "Pack a sun shelter!"
+#  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+# else
+#  puts "Stay at home and read Hegel."
+# end
+
+# puts "You know what year it is??"
+# this_year = 2019
+# puts "Hey, it's 2019!" if this_year == 2019
+
+current_time = Time.now
+current_time = current_time.to_i
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+  
+
