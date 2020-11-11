be641da777ffd62601f1c5d75853928690552c33
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-11 18:46:50 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
