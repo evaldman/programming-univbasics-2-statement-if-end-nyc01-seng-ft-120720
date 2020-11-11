10de495db32e70e541b9892f766e235fdd8ab61b
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-11 18:49:22 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,3 +4,12 @@
 #   puts "code inside"
 # end
 # puts "Code after if...end"
+
+chance_of_rain = 0.2
+puts "Let's go outside"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen"
